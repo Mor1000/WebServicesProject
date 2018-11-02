a2bfbc59316a271cacd63079bdd76320eacba525
Computer <mor.tabuch10@gmail.com>
2018-11-02 15:18:35 +0200

    Organizing and documenting the Login and Register classes.

--- FinalWebProject/FinalWebProject/Login.aspx.cs
+++ FinalWebProject/FinalWebProject/Login.aspx.cs
@@ -8,30 +8,48 @@ using System.Web.UI.WebControls;
 
 namespace FinalWebProject
 {
+    /// <summary>
+    /// This is the code behind of the users login page.
+    /// </summary>
     public partial class Login : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
 
         }
-
+        /// <summary>
+        /// A method called when the user clicked the login button. If the username and password
+        /// are valid and exists in the users table in the databese the user 
+        /// should be successfully signed in. 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         protected void LoginClick(object sender, EventArgs e)
         {
+            /*loading all the validators from the "loginValidation" validation group
+                * and checking if they are all valid.*/
             Validate("loginValidation");
             if (IsValid)
+                //The database connection in the using block will be automatically closed in any event.      
                 using (OleDbConnection conn = new OleDbConnection(Connection.GetConnectionString()))
                 {
-                    string query = "SELECT UserName, UserPassword FROM Users where userName = @user and UserPassword = @password";
+                    string query = "SELECT UserName, UserPassword FROM Users where userName = @user and UserPassword = @password"; ;//This query is parameterized so that the user input will be checked only as one of the fields in the table.
                     OleDbCommand command = new OleDbCommand(query, conn);
+                    //defining the query's parameters.
                     command.Parameters.AddWithValue("@user", userTextBox.Text);
                     command.Parameters.AddWithValue("@password", passwordTextBox.Text);
                     conn.Open();
-                    OleDbDataReader reader = command.ExecuteReader();
-                    if (!reader.Read())
+                    OleDbDataReader reader = command.ExecuteReader();//executing the query.
+                    if (!reader.Read())//check if the data was found in the database. 
                         Response.Write("<script>alert('Login failed');</script>");
                 }
         }
 
+        /// <summary>
+        /// switching to the registration page when the sign up button is clicked.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         protected void SignUpClick(object sender, EventArgs e)
         {
             Response.Redirect("~/Register.aspx");
--- FinalWebProject/FinalWebProject/Register.aspx.cs
+++ FinalWebProject/FinalWebProject/Register.aspx.cs
@@ -8,6 +8,9 @@ using System.Web.UI.WebControls;
 
 namespace FinalWebProject
 {
+    /// <summary>
+    /// This is the code behind of the users registration page.
+    /// </summary>
     public partial class Register : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
@@ -15,6 +18,11 @@ namespace FinalWebProject
 
         }
 
+        /// <summary>
+        /// clearing all the input fields.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         protected void OnClearClick(object sender, EventArgs e)
         {
             userTextBox.Text = "";
@@ -27,17 +35,28 @@ namespace FinalWebProject
 
         }
 
+        /// <summary>
+        /// A method called when the user clicked the sign up button. If all 
+        /// of the input data is valid, the user is registered the login page should be loaded
+        ///and the user details are added to the users the users table in the databese.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         protected void SignUpClick(object sender, EventArgs e)
         {
 
             if (!IsAlreadyExistsUser())
             {
-                Validate("signUp");
-                if (IsValid)
+                /*loading all the validators from the "signUp" validation group
+                 * and checking if they are all valid.*/
+                Validate("signUp"); 
+                if (IsValid)   
+                    //The database connection in the using block will be automatically closed in any event.      
                     using (OleDbConnection conn = new OleDbConnection(Connection.GetConnectionString()))
                     {
-                        string query = "INSERT INTO Users VALUES(@user, @password, @email, @birthdate, @name, @lastName)";
+                        string query = "INSERT INTO Users VALUES(@user, @password, @email, @birthdate, @name, @lastName)";//This query is parameterized so that the user input will be checked only as one of the fields in the table.
                         OleDbCommand command = new OleDbCommand(query, conn);
+                        //defining the query's parameters.
                         command.Parameters.AddWithValue("@user", userTextBox.Text);
                         command.Parameters.AddWithValue("@password", passwordTextBox.Text);
                         command.Parameters.AddWithValue("@email", emailTextBox.Text);
@@ -45,17 +64,25 @@ namespace FinalWebProject
                         command.Parameters.AddWithValue("@name", nameTextBox.Text);
                         command.Parameters.AddWithValue("@lastName", lastNameTextBox.Text);
                         conn.Open();
-                        int reader = command.ExecuteNonQuery();
+                        int reader = command.ExecuteNonQuery();//executing the query. the method returns the number of lines inserted.
                         if (reader > 0)
                         {
-                            Response.Write("<script> alert('Successfuly registered');</script>");
-                            Response.Redirect("~/Login.aspx");
+                            Response.Write("<script> alert('Successfuly registered');</script>"); //writing in a popup window message.
+                            Response.Redirect("~/Login.aspx"); //switching to the login page.
                         }
                     }
             }
             else
                 Response.Write("<script> alert('User already taken');</script>");
         }
+        /// <summary>
+        /// This method checked if the username that was entered is already exists
+        /// in the users table in the database.
+        /// </summary>
+        /// <returns>
+        /// If the username exists this methot returns true,
+        /// otherwise it returns false.
+        /// </returns>
         public bool IsAlreadyExistsUser()
         {
             using (OleDbConnection conn = new OleDbConnection(Connection.GetConnectionString()))
@@ -70,6 +97,12 @@ namespace FinalWebProject
                 return false;
             }
         }
+
+        /// <summary>
+        /// This method checks whether the username and the password are valid or not.
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="args"></param>
         protected void CheckChars(object source, ServerValidateEventArgs args)
         {
             args.IsValid = args.Value.Length >= 5;
